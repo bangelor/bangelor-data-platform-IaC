9e82082138dcf561b61fec9bd664e5718cb03447
LBN <lorenz.bangerter@isolutions.ch>
2025-03-13 18:00:48 +0100

    added container

--- main.tf
+++ main.tf
@@ -63,6 +63,25 @@ resource "azurerm_container_registry" "azcoreg" {
   sku                 = "Premium"
 }
 
+resource "azurerm_container_group" "example" {
+  name                = "example-containergroup"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  os_type             = "Linux"
+
+  container {
+    name   = "example-container"
+    image  = "${azurerm_container_registry.azcoreg.login_server}/my-image:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+}
+
 resource "azurerm_role_assignment" "example" {
   principal_id                     = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
