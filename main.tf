e4ab51ad427f98dd6bc76b0f47564548fa882a03
LBN <lorenz.bangerter@isolutions.ch>
2025-03-13 18:02:10 +0100

    bugfix

--- main.tf
+++ main.tf
@@ -65,8 +65,8 @@ resource "azurerm_container_registry" "azcoreg" {
 
 resource "azurerm_container_group" "example" {
   name                = "example-containergroup"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
 
   container {
