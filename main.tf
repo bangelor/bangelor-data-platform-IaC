b5fed33423ac31f67dec4126facf2ea4c74743c9
LBN <lorenz.bangerter@isolutions.ch>
2025-03-13 12:26:32 +0100

    add container registry

--- main.tf
+++ main.tf
@@ -21,6 +21,8 @@ resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
   prefix = "dns"
 }
 
+
+
 resource "azurerm_kubernetes_cluster" "k8s" {
   location            = azurerm_resource_group.rg.location
   name                = random_pet.azurerm_kubernetes_cluster_name.id
@@ -47,4 +49,22 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     network_plugin    = "kubenet"
     load_balancer_sku = "standard"
   }
+}
+
+resource "random_pet" "azurerm_container_registry" {
+  prefix = "azcoreg"
+}
+
+resource "azurerm_container_registry" "azcoreg" {
+  name                = random_pet.azurerm_container_registry.id
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Premium"
+}
+
+resource "azurerm_role_assignment" "example" {
+  principal_id                     = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.azcoreg.example.id
+  skip_service_principal_aad_check = true
 }
