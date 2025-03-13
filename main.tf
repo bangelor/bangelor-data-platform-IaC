291cd81843339d1750e9392a9b5c3cd7d99cb061
LBN <lorenz.bangerter@isolutions.ch>
2025-03-13 12:29:22 +0100

    bugfix

--- main.tf
+++ main.tf
@@ -65,6 +65,6 @@ resource "azurerm_container_registry" "azcoreg" {
 resource "azurerm_role_assignment" "example" {
   principal_id                     = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.azcoreg.example.id
+  scope                            = azurerm_container_registry.azcoreg.azcoreg.id
   skip_service_principal_aad_check = true
 }
